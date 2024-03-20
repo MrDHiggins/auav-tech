3e5b3b8784ed0f32f64ced3ead06e1758c929ac3
Dyllan Fabio Higgins <dyllanhiggins7@gmail.com>
2024-03-20 03:32:47 +0200

    Added Navigation menu UI components

---
+++ app/ui/navigation/navItems.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+import {
+    HomeIcon,
+    BuildingStorefrontIcon,
+    InboxIcon,
+    BookmarkIcon
+} from '@heroicons/react/24/solid';
+
+const itemLinks = [
+    {name: 'Overview', href: '/auavTechHome', icon: HomeIcon},
+    {name: 'Services Offered', href: '/auavTechHome/services', icon: InboxIcon},//Must be
+    {name: 'Operations', href: '/auavTechHome/operations', icon: InboxIcon},
+    {name: 'Company', href: '/auavTechHome/company', icon: BuildingStorefrontIcon},
+    {name: 'Blog', href: '/auavTechHome/blog', icon: BookmarkIcon},
+    {name: 'Contact', href: '/auavTechHome/contact', icon: InboxIcon},
+];
+
+
+export default function NavItems() {
+    const pathName = usePathname();
+  return (
+    <>
+    {itemLinks.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                'flex h-[40px] m-1 grow items-center justify-center gap-2   p-3 text-sm font-medium hover:bg-sky-100 hover:text-gray-600 md:flex-row md:justify-center md:p-2 md:px-3',
+                {
+                  'text-black': pathName === link.href,
+                },
+              )}
+            >
+                <LinkIcon className='flex flex-row h-6 w-6' />
+                <p className='hidden md:block'>{link.name}</p>
+            </Link>
+        );
+    }
+    )}
+    </>
+  );
+}
---
+++ app/ui/navigation/topNav.tsx
@@ -0,0 +1,12 @@
+
+import NavItems from '@/app/ui/navigation/navItems';
+
+export default function TopNav() {
+    return (
+        <div className='w-full h-12 bg-red-700'>
+            <div className='flex'>
+            <NavItems />
+            </div>
+        </div>
+    );
+    }
