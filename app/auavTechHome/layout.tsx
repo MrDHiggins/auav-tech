4438ab6dcaee7a6cb26622d5ba53ab275b10b192
Dyllan Fabio Higgins <dyllanhiggins7@gmail.com>
2024-03-22 05:21:41 +0200

    Removed backgroundImage UI from auavtechhome

--- app/auavTechHome/layout.tsx
+++ app/auavTechHome/layout.tsx
@@ -1,5 +1,4 @@
 import TopNav from "../ui/navigation/topNav";
-import BackgroundImage from '@/app/ui/overview/backgroundImage';
 
 export default function Layout ({ children }: { children: React.ReactNode }) {
     return (
