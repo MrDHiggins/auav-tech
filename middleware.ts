619754ae24c642db324ab793db570ca7dbe454b1
Dyllan Fabio Higgins <dyllanhiggins7@gmail.com>
2024-03-22 05:20:38 +0200

    Remove middleware - need to resolve 404 issue

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { get } from '@vercel/edge-config'
 
-export const config = {
-    matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       */
-      '/((?!api|_next/static|_next/image|favicon.ico).*)'
-    ]
-  }
-  export async function middleware(req: NextRequest) {
-    if (!process.env.EDGE_CONFIG) {
-      req.nextUrl.pathname = `/missing-edge-config`
-      return NextResponse.rewrite(req.nextUrl)
-    }
-  
-    try {
-      // Check whether the maintenance page should be shown
-      const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
-  
-      // If is in maintenance mode, point the url pathname to the maintenance page
-      if (isInMaintenanceMode) {
-        req.nextUrl.pathname = `/maintenance`
-  
-        // Rewrite to the url
-        return NextResponse.rewrite(req.nextUrl)
-      }
-    } catch (error) {
-      // show the default page if EDGE_CONFIG env var is missing,
-      // but log the error to the console
-      console.error(error)
-    }
-  }
+// export const config = {
+//     matcher: [
+//       /*
+//        * Match all request paths except for the ones starting with:
+//        * - api (API routes)
+//        * - _next/static (static files)
+//        * - _next/image (image optimization files)
+//        * - favicon.ico (favicon file)
+//        */
+//       '/((?!api|_next/static|_next/image|favicon.ico).*)'
+//     ]
+//   }
+export async function middleware(req: NextRequest) {
+  // Check Edge Config to see if the maintenance page should be shown
+//   const isInMaintenanceMode = await get<boolean>('isInMaintenanceMode')
+
+//   // If in maintenance mode, point the url pathname to the maintenance page
+//   if (isInMaintenanceMode) {
+//     req.nextUrl.pathname = `/maintenance`
+
+//     // Rewrite to the url
+//     return NextResponse.rewrite(req.nextUrl)
+//   }
+}
