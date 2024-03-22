004a2623d04813d03b5674cbcc007196ac0b1dcc
Dyllan Fabio Higgins <dyllanhiggins7@gmail.com>
2024-03-22 05:22:25 +0200

    Added UI components for partners & services

---
+++ app/ui/partners/index.tsx
@@ -0,0 +1,9 @@
+export default function partners() {
+    return (
+        <div className="flex h-full">
+            <div className="flex w-full bg-blue-700">
+                <h1 className="flex h-1/3">partners</h1>
+            </div>
+        </div>
+    );
+}
---
+++ app/ui/services/index.tsx
@@ -0,0 +1,9 @@
+export default function services() {
+    return (
+        <div className="flex h-full">
+            <div className="flex w-full bg-orange-700 ">
+                <h1 className="flex">Services</h1>
+            </div>
+        </div>
+    );
+}
