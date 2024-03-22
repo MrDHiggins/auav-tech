a71d76413637758787e86165087f5a698786cab6
Dyllan Fabio Higgins <dyllanhiggins7@gmail.com>
2024-03-22 05:21:12 +0200

    Reworked nav: Sticky nav and adjusted navItems styling

--- app/ui/navigation/navItems.tsx
+++ app/ui/navigation/navItems.tsx
@@ -13,7 +13,7 @@ import {
 
 const itemLinks = [
     {name: 'Overview', href: '/auavTechHome', icon: HomeIcon},
-    {name: 'Services Offered', href: '/auavTechHome/services', icon: InboxIcon},//Must be
+    {name: 'Services', href: '/auavTechHome/services', icon: InboxIcon},//Must be
     {name: 'Operations', href: '/auavTechHome/operations', icon: InboxIcon},
     {name: 'Company', href: '/auavTechHome/company', icon: BuildingStorefrontIcon},
     {name: 'Blog', href: '/auavTechHome/blog', icon: BookmarkIcon},
@@ -24,26 +24,34 @@ const itemLinks = [
 export default function NavItems() {
     const pathName = usePathname();
   return (
-    <>
-    {itemLinks.map((link) => {
+<>
+  <div className="flex items-center justify-between w-full"> 
+    <div>
+      <h1 className="ml-2">AUAVTech</h1>
+    </div>
+
+    <div className="flex justify-end">
+      {itemLinks.map((link) => {
         const LinkIcon = link.icon;
         return (
+          <div key={link.name}> 
             <Link
-              key={link.name}
               href={link.href}
               className={clsx(
-                'flex h-[40px] m-1 grow items-center justify-center gap-2   p-3 text-sm font-medium hover:bg-sky-100 hover:text-gray-600 md:flex-row md:justify-center md:p-2 md:px-3',
-                {
-                  'text-black': pathName === link.href,
-                },
+                'flex h-[40px] w-30 m-1 items-center justify-center shrink gap-2 p-3 text-sm font-medium hover:underline text md:flex-row md:justify-center md:p-2 md:px-3',
+                { 
+                  'text-black': pathName === link.href
+                }
               )}
             >
-                <LinkIcon className='flex flex-row h-6 w-6' />
-                <p className='hidden md:block'>{link.name}</p>
+              <LinkIcon className='flex flex-row h-6 w-6' />
+              <p className='hidden md:block'>{link.name}</p>
             </Link>
+          </div>
         );
-    }
-    )}
-    </>
-  );
+      })}
+    </div>
+  </div>
+</>
+);
 }
--- app/ui/navigation/topNav.tsx
+++ app/ui/navigation/topNav.tsx
@@ -3,10 +3,10 @@ import NavItems from '@/app/ui/navigation/navItems';
 
 export default function TopNav() {
     return (
-        <div className='w-full h-12 bg-red-700'>
-            <div className='flex'>
-            <NavItems />
-            </div>
+        <div className='h-12 bg-red-700 m-0 fixed top-0 w-full z-10'> 
+        <div className='flex justify-end'>
+          <NavItems />
         </div>
+      </div>
     );
     }
